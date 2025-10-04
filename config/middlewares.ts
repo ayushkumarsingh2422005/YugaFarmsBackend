55b9ec28808aaba4c5f8d8ee287a2215209e0636
ayushkumarsingh2422005 <ayush2422005@gmail.com>
2025-10-04 23:39:03 +0530

    deployemtn files added

--- config/middlewares.ts
+++ config/middlewares.ts
@@ -1,6 +1,29 @@
 export default [
   'strapi::logger',
   'strapi::errors',
+  {
+    name: 'strapi::security',
+    config: {
+      contentSecurityPolicy: {
+        useDefaults: true,
+        directives: {
+          'img-src': [
+            "'self'",
+            'data:',
+            'blob:',
+            'https://market-assets.strapi.io',
+          ],
+          'media-src': [
+            "'self'",
+            'data:',
+            'blob:',
+          ],
+          'connect-src': ["'self'", 'https:'],
+          upgradeInsecureRequests: null,
+        },
+      },
+    },
+  },
   'strapi::security',
   'strapi::cors',
   'strapi::poweredBy',
