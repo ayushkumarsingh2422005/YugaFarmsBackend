6ce95eae237cec4cb1f5328a4f5461da66a9327a
ayushkumarsingh2422005 <ayush2422005@gmail.com>
2025-11-04 10:26:33 +0530

    added the chages of smtp

--- config/plugins.ts
+++ config/plugins.ts
@@ -1,1 +1,15 @@
-export default () => ({});
+export default ({ env }) => ({
+    email: {
+        config: {
+            provider: 'strapi-provider-email-brevo',
+            providerOptions: {
+                apiKey: env('BREVO_API_KEY'), // Use environment variable for security
+            },
+            settings: {
+                defaultSenderEmail: env('EMAIL_FROM'),
+                defaultSenderName: env('EMAIL_FROM_NAME'),
+                defaultReplyTo: env('EMAIL_REPLY_TO'),
+            },
+        },
+    },
+});
