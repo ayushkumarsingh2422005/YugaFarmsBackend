9febf41ed4229ee1f23246f612a95d5ff57f6010
ayushkumarsingh2422005 <ayush2422005@gmail.com>
2026-01-14 23:23:35 +0530

    sataus added

---
+++ src/api/nav-offer/controllers/nav-offer.ts
@@ -0,0 +1,8 @@
+/**
+ * nav-offer controller
+ */
+
+import { factories } from '@strapi/strapi'
+
+// @ts-ignore
+export default factories.createCoreController('api::nav-offer.nav-offer');
---
+++ src/api/nav-offer/services/nav-offer.ts
@@ -0,0 +1,8 @@
+/**
+ * nav-offer service
+ */
+
+import { factories } from '@strapi/strapi';
+
+// @ts-ignore
+export default factories.createCoreService('api::nav-offer.nav-offer');
